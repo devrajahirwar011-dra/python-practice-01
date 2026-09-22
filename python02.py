aaff5a05b8091ddff2bfaad2bc45ad918fb0c866
devrajahirwar011-dra <devrajahirwar011@gmail.com>
2026-09-22 17:26:10 +0530

    Implement list operations and print results
    Created a list of 8 numbers and performed various operations including printing its length, max, min, sorted version, appending a new number, inserting a number at index 2, and printing the list after each change.

---
+++ python02.py
@@ -0,0 +1,11 @@
+# wap to create a list of 8 numbers. print its length max min and sorted version then append a new number insert a number at index. 2 remove a number and finally reverse the list- printing the list after every change.
+
+my_list = [1,2,3,4,5,6,7,8 ]
+print("Length",len(my_list))
+print("max",max(my_list))
+print("Min",min(my_list))
+print("Sorted List",sorted(my_list))
+my_list.append("new")
+print("append",my_list)
+my_list.insert(2,8)
+print("final List",my_list)
