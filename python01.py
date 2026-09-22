54afa8a9d1e1b0ffe10596fad44e88e9efa50757
devrajahirwar011-dra <devrajahirwar011@gmail.com>
2026-09-22 17:21:33 +0530

    Add list slicing examples for scores
    Implement list slicing to display various segments of scores.

---
+++ python01.py
@@ -0,0 +1,7 @@
+#WAP TO CREATE A LIST MATCH scores with 10 integer scores. using slicing print. (a) the first 4 scores (b) the last four scores (c) every alternate score. (d) the whole list reversed.
+
+my_list = [1,2,3,4,5,6,7,8,9,10 ]
+print("slice:",my_list[0:4])
+print("slice:",my_list[6:])
+print("slice:",my_list[0:9:2])
+print("slice:",my_list[::-1])
